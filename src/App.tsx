1c0352c212b470684257de4523c2641ee64b6f78
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-02-21 13:17:15 +0100

    Remove index.css remove unnecessary state from app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { useState } from "react";
 import Child from "./Child";
 import "./App.css";
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 function App() {
-  const [counts, setCounts] = useState(numbers);
   return (
     <>
-      {counts.map((count) => {
+      {numbers.map((count) => {
         return <Child count={count} key={count} />;
       })}
     </>
